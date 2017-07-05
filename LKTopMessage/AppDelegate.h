bb8910c2f5d9efb0d33f5cc74d6072202fca7ead
karos <329847738@qq.com>
2017-07-05 22:44:38 +0800

    Initial Commit

---
+++ LKTopMessage/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LKTopMessage
+//
+//  Created by karos li on 2017/7/5.
+//  Copyright © 2017年 karos. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LKTopMessage/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LKTopMessage
+//
+//  Created by karos li on 2017/7/5.
+//  Copyright © 2017年 karos. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
